9733e4cbb6ea562a0cc7764f4f4d825222df42e9
craigcarlson <ccarlson@mines.edu>
2014-02-05 02:28:55 -0700

    Removed old files

--- HumanSociety/app/views/pets/index.json.jbuilder
+++
@@ -1,4 +0,0 @@
-json.array!(@pets) do |pet|
-  json.extract! pet, :id, :species, :name, :description, :image_url, :age
-  json.url pet_url(pet, format: :json)
-end
--- HumanSociety/config/initializers/session_store.rb
+++
@@ -1,3 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-HumanSociety::Application.config.session_store :cookie_store, key: '_HumanSociety_session'
