5a522cf7aff4c4268c89af905a2ebc202e9606b5
craigcarlson <ccarlson@mines.edu>
2014-02-05 03:14:19 -0700

    Revert "Added unit tests"
    This reverts commit 4af722492d862aa2d85f6ee4bd9674a3c1923983.

--- humaneSociety/app/models/pet.rb
+++ humaneSociety/app/models/pet.rb
@@ -1,6 +1,2 @@
 class Pet < ActiveRecord::Base
-	validates :name, :description, :image_url, :presence => true
-	validates :age, :numericality => {:greater_than_or_equal_to => 0.01}
-	validates :name, :uniqueness => true
-	
 end
